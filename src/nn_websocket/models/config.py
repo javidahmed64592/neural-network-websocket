380e25db1e2e6327bbcb7696ef2f469ae0b60d3d
Javid Ahmed <javidahmed@icloud.com>
2025-05-29 21:33:20 +0100

    Refactor Config class to remove default values and implement load_config method for dynamic configuration loading

--- src/nn_websocket/models/config.py
+++ src/nn_websocket/models/config.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+import json
+import os
+from pathlib import Path
+
 from pydantic.dataclasses import dataclass
 
 
@@ -7,5 +13,11 @@ class Config:
     Configuration for the WebSocket server.
     """
 
-    host: str = "localhost"
-    port: int = 8765
+    host: str
+    port: int
+
+    @staticmethod
+    def load_config(filepath: os.PathLike) -> Config:
+        with Path(filepath).open() as f:
+            config_dict = json.load(f)
+        return Config(**config_dict)
--- tests/nn_websocket/models/test_config.py
+++ tests/nn_websocket/models/test_config.py
@@ -2,14 +2,6 @@ from nn_websocket.models.config import Config
 
 
 class TestConfig:
-    def test_default_config(self) -> None:
-        """Test the default configuration values."""
-        default_host = "localhost"
-        default_port = 8765
-        config = Config()
-        assert config.host == default_host
-        assert config.port == default_port
-
     def test_custom_config(self) -> None:
         """Test custom configuration values."""
         custom_host = "custom_host"
