25281c691df35e3deca9a67799a4322e3e3ad389
Javid Ahmed <javidahmed@icloud.com>
2025-05-31 05:06:50 +0100

    Begin updating to use neuroevolution algorithm

--- src/nn_websocket/main.py
+++ src/nn_websocket/main.py
@@ -6,40 +6,47 @@ import websockets
 
 from nn_websocket.models.config import Config
 from nn_websocket.models.nn_suite import NeuralNetworkSuite
+from nn_websocket.protobuf.proto_types import ActionData, FrameRequestData, ObservationData, PopulationFitnessData
 
 CONFIG_FILEPATH = Path("config") / "websocket_config.json"
 
 logging.basicConfig(format="%(asctime)s %(message)s", datefmt="[%d-%m-%Y|%I:%M:%S]", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-neural_network_suite = NeuralNetworkSuite()
 
 
 async def handle_connection(websocket: websockets.ServerConnection) -> None:
-    num_messages = 0
+    neural_network_suite: NeuralNetworkSuite | None = None
     async for message in websocket:
-        logger.info("Received message of length: %s", len(message))
         if isinstance(message, str):
-            logger.info("Message is a string, encoding to bytes.")
             message_bytes = message.encode("utf-8")
         else:
             message_bytes = message
-        if num_messages == 0:
-            logger.info("Configuring neural networks with the first message.")
-            configure_neural_networks(message_bytes)
+        if neural_network_suite is None:
+            neural_network_suite = configure_neural_networks(message_bytes)
         else:
-            await process_observations(websocket, message_bytes)
-        num_messages += 1
+            message_data = FrameRequestData.from_bytes(message_bytes)
+            if population_fitness := message_data.population_fitness:
+                crossover_neural_networks(neural_network_suite, population_fitness)
+            elif observation := message_data.observation:
+                actions = process_observations(neural_network_suite, observation)
+                await websocket.send(actions.to_bytes(actions))
 
 
-def configure_neural_networks(config: bytes) -> None:
-    neural_network_suite.set_networks_from_bytes(config)
-    logger.info("Configured %s neural networks with shared settings.", len(neural_network_suite.networks))
+def configure_neural_networks(config: bytes) -> NeuralNetworkSuite:
+    logger.info("Configuring neural networks...")
+    return NeuralNetworkSuite.from_bytes(config)
 
 
-async def process_observations(websocket: websockets.ServerConnection, message: bytes) -> None:
-    actions = neural_network_suite.feedforward_through_networks_from_bytes(message)
-    logger.info("Calculated actions: %s", actions.outputs)
-    await websocket.send(actions.to_bytes(actions))
+def crossover_neural_networks(
+    neural_network_suite: NeuralNetworkSuite, population_fitness: PopulationFitnessData
+) -> None:
+    logger.info("Crossover neural networks...")
+    # TODO: Implement the crossover logic for neural networks based on fitness data
+    pass
+
+
+def process_observations(neural_network_suite: NeuralNetworkSuite, observation: ObservationData) -> ActionData:
+    return neural_network_suite.feedforward_through_networks(observation)
 
 
 async def main() -> None:
