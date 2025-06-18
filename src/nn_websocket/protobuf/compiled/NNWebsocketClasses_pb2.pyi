d042b708f5fc7cef15619ec10e9afa1d6200fec2
Javid Ahmed <javidahmed@icloud.com>
2025-06-19 00:32:55 +0100

    Refactor neural network protobuf definitions and update activation function handling

--- src/nn_websocket/protobuf/compiled/NNWebsocketClasses_pb2.pyi
+++ src/nn_websocket/protobuf/compiled/NNWebsocketClasses_pb2.pyi
@@ -1,5 +1,5 @@
+from . import NeuralNetwork_pb2 as _NeuralNetwork_pb2
 from google.protobuf.internal import containers as _containers
-from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from collections.abc import Iterable as _Iterable, Mapping as _Mapping
@@ -7,19 +7,8 @@ from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class ActivationFunctionEnum(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
-    __slots__ = ()
-    LINEAR: _ClassVar[ActivationFunctionEnum]
-    RELU: _ClassVar[ActivationFunctionEnum]
-    SIGMOID: _ClassVar[ActivationFunctionEnum]
-    TANH: _ClassVar[ActivationFunctionEnum]
-LINEAR: ActivationFunctionEnum
-RELU: ActivationFunctionEnum
-SIGMOID: ActivationFunctionEnum
-TANH: ActivationFunctionEnum
-
 class NeuralNetworkConfig(_message.Message):
-    __slots__ = ("num_inputs", "num_outputs", "hidden_layer_sizes", "weights_min", "weights_max", "bias_min", "bias_max", "input_activation", "hidden_activation", "output_activation", "learning_rate")
+    __slots__ = ("num_inputs", "num_outputs", "hidden_layer_sizes", "weights_min", "weights_max", "bias_min", "bias_max", "input_activation", "hidden_activation", "output_activation", "optimizer")
     NUM_INPUTS_FIELD_NUMBER: _ClassVar[int]
     NUM_OUTPUTS_FIELD_NUMBER: _ClassVar[int]
     HIDDEN_LAYER_SIZES_FIELD_NUMBER: _ClassVar[int]
@@ -30,7 +19,7 @@ class NeuralNetworkConfig(_message.Message):
     INPUT_ACTIVATION_FIELD_NUMBER: _ClassVar[int]
     HIDDEN_ACTIVATION_FIELD_NUMBER: _ClassVar[int]
     OUTPUT_ACTIVATION_FIELD_NUMBER: _ClassVar[int]
-    LEARNING_RATE_FIELD_NUMBER: _ClassVar[int]
+    OPTIMIZER_FIELD_NUMBER: _ClassVar[int]
     num_inputs: int
     num_outputs: int
     hidden_layer_sizes: _containers.RepeatedScalarFieldContainer[int]
@@ -38,11 +27,11 @@ class NeuralNetworkConfig(_message.Message):
     weights_max: float
     bias_min: float
     bias_max: float
-    input_activation: ActivationFunctionEnum
-    hidden_activation: ActivationFunctionEnum
-    output_activation: ActivationFunctionEnum
-    learning_rate: float
-    def __init__(self, num_inputs: _Optional[int] = ..., num_outputs: _Optional[int] = ..., hidden_layer_sizes: _Optional[_Iterable[int]] = ..., weights_min: _Optional[float] = ..., weights_max: _Optional[float] = ..., bias_min: _Optional[float] = ..., bias_max: _Optional[float] = ..., input_activation: _Optional[_Union[ActivationFunctionEnum, str]] = ..., hidden_activation: _Optional[_Union[ActivationFunctionEnum, str]] = ..., output_activation: _Optional[_Union[ActivationFunctionEnum, str]] = ..., learning_rate: _Optional[float] = ...) -> None: ...
+    input_activation: _NeuralNetwork_pb2.ActivationFunctionData
+    hidden_activation: _NeuralNetwork_pb2.ActivationFunctionData
+    output_activation: _NeuralNetwork_pb2.ActivationFunctionData
+    optimizer: _NeuralNetwork_pb2.OptimizerData
+    def __init__(self, num_inputs: _Optional[int] = ..., num_outputs: _Optional[int] = ..., hidden_layer_sizes: _Optional[_Iterable[int]] = ..., weights_min: _Optional[float] = ..., weights_max: _Optional[float] = ..., bias_min: _Optional[float] = ..., bias_max: _Optional[float] = ..., input_activation: _Optional[_Union[_NeuralNetwork_pb2.ActivationFunctionData, str]] = ..., hidden_activation: _Optional[_Union[_NeuralNetwork_pb2.ActivationFunctionData, str]] = ..., output_activation: _Optional[_Union[_NeuralNetwork_pb2.ActivationFunctionData, str]] = ..., optimizer: _Optional[_Union[_NeuralNetwork_pb2.OptimizerData, _Mapping]] = ...) -> None: ...
 
 class Configuration(_message.Message):
     __slots__ = ("neuroevolution", "fitness_approach")
